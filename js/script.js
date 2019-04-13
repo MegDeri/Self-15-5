43d1d0631e5d2d7bb76f5530706ef1c1f7bb2ca2
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-04-13 23:39:00 +0300

    Final change js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 function Button(text) {
-	this.text = text || 'Hello';
+  this.text = text || 'Hello';
 }
 
 Button.prototype = {
-	create: function() {
-        var self = this;
-        this.element = document.createElement('button');
-        this.element.innerText = this.text;
-        this.element.addEventListener('click', function() {
-            alert(self.text);
-        });
-        document.body.appendChild(this.element);
-    }
+  create: function() {
+    var self = this;
+    this.element = document.createElement('button');
+    this.element.innerText = this.text;
+    this.element.addEventListener('click', function() {
+      alert(self.text);
+    });
+    document.body.appendChild(this.element);
+  }
 }
 
 var btn1 = new Button('Hello!');
